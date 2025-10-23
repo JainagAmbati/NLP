61b95c0900a9f7841b1647f435bc126965265d27
JainagAmbati <jainagambati@gmail.com>
2025-10-23 15:18:27 +0000

    update

--- a4-distrib/factcheck.py
+++ a4-distrib/factcheck.py
@@ -76,81 +76,81 @@ class AlwaysEntailedFactChecker(FactChecker):
         return "S"
 
 
-# class WordRecallThresholdFactChecker(FactChecker):
-#     def __init__(self, threshold=0.55):
-#         self.threshold = threshold
-#         self.nlp = spacy.load("en_core_web_sm", disable=["ner", "parser"])
-
-#     def _tokenize(self, text):
-#         doc = self.nlp(text.lower())
-#         tokens = [t.lemma_ for t in doc if t.is_alpha and not t.is_stop]
-#         return set(tokens)
-
-#     def _recall_overlap(self, fact_tokens, sent_tokens):
-#         if not fact_tokens:
-#             return 0.0
-#         return len(fact_tokens & sent_tokens) / len(fact_tokens)
-
-#     def predict(self, fact: str, passages: List[dict]) -> str:
-#         fact_tokens = self._tokenize(fact)
-#         if not fact_tokens:
-#             return "NS"
-#         best_score = 0.0
-#         for passage in passages:
-#             sents = re.split(r'(?<=[.!?])\s+', passage["text"])
-#             for sent in sents:
-#                 sent_tokens = self._tokenize(sent)
-#                 if not sent_tokens:
-#                     continue
-#                 score = self._recall_overlap(fact_tokens, sent_tokens)
-#                 best_score = max(best_score, score)
-#         return "S" if best_score >= self.threshold else "NS"
-
-from nltk.corpus import wordnet as wn
-
 class WordRecallThresholdFactChecker(FactChecker):
-    def __init__(self, threshold=0.6):
+    def __init__(self, threshold=0.65):
         self.threshold = threshold
         self.nlp = spacy.load("en_core_web_sm", disable=["ner", "parser"])
 
-    def _expand_synonyms(self, lemma):
-        syns = {lemma}
-        for syn in wn.synsets(lemma):
-            for l in syn.lemmas():
-                s = l.name().replace("_", " ").lower()
-                if s.isalpha() and len(s) > 2:
-                    syns.add(s)
-        return syns
-
     def _tokenize(self, text):
         doc = self.nlp(text.lower())
-        toks = []
-        for t in doc:
-            if not t.is_alpha:
-                continue
-            lemma = t.lemma_
-            toks.extend(list(self._expand_synonyms(lemma)))
-        return set(toks)
+        tokens = [t.lemma_ for t in doc if t.is_alpha and not t.is_stop]
+        return set(tokens)
 
-    def _f1_overlap(self, fact_set, sent_set):
-        if not fact_set or not sent_set:
+    def _recall_overlap(self, fact_tokens, sent_tokens):
+        if not fact_tokens:
             return 0.0
-        inter = fact_set & sent_set
-        prec = len(inter) / len(sent_set)
-        rec = len(inter) / len(fact_set)
-        return 2 * prec * rec / (prec + rec + 1e-9)
+        return len(fact_tokens & sent_tokens) / len(fact_tokens)
 
     def predict(self, fact: str, passages: List[dict]) -> str:
         fact_tokens = self._tokenize(fact)
         if not fact_tokens:
             return "NS"
-
         best_score = 0.0
         for passage in passages:
-            for sent in re.split(r'(?<=[.!?])\s+', passage["text"]):
+            sents = re.split(r'(?<=[.!?])\s+', passage["text"])
+            for sent in sents:
                 sent_tokens = self._tokenize(sent)
-                best_score = max(best_score, self._f1_overlap(fact_tokens, sent_tokens))
+                if not sent_tokens:
+                    continue
+                score = self._recall_overlap(fact_tokens, sent_tokens)
+                best_score = max(best_score, score)
         return "S" if best_score >= self.threshold else "NS"
+
+# from nltk.corpus import wordnet as wn
+
+# class WordRecallThresholdFactChecker(FactChecker):
+#     def __init__(self, threshold=0.3):
+#         self.threshold = threshold
+#         self.nlp = spacy.load("en_core_web_sm", disable=["ner", "parser"])
+
+#     def _expand_synonyms(self, lemma):
+#         syns = {lemma}
+#         for syn in wn.synsets(lemma):
+#             for l in syn.lemmas():
+#                 s = l.name().replace("_", " ").lower()
+#                 if s.isalpha() and len(s) > 2:
+#                     syns.add(s)
+#         return syns
+
+#     def _tokenize(self, text):
+#         doc = self.nlp(text.lower())
+#         toks = []
+#         for t in doc:
+#             if not t.is_alpha:
+#                 continue
+#             lemma = t.lemma_
+#             toks.extend(list(self._expand_synonyms(lemma)))
+#         return set(toks)
+
+#     def _f1_overlap(self, fact_set, sent_set):
+#         if not fact_set or not sent_set:
+#             return 0.0
+#         inter = fact_set & sent_set
+#         prec = len(inter) / len(sent_set)
+#         rec = len(inter) / len(fact_set)
+#         return 2 * prec * rec / (prec + rec + 1e-9)
+
+#     def predict(self, fact: str, passages: List[dict]) -> str:
+#         fact_tokens = self._tokenize(fact)
+#         if not fact_tokens:
+#             return "NS"
+
+#         best_score = 0.0
+#         for passage in passages:
+#             for sent in re.split(r'(?<=[.!?])\s+', passage["text"]):
+#                 sent_tokens = self._tokenize(sent)
+#                 best_score = max(best_score, self._f1_overlap(fact_tokens, sent_tokens))
+#         return "S" if best_score >= self.threshold else "NS"
 
 
 class EntailmentFactChecker(FactChecker):
